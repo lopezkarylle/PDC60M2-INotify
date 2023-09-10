58377addd5ef2d97b9cf491431282e0e19b4390b
lopezkarylle <dr.lopezkarylle@gmail.com>
2023-09-10 08:14:37 +0800

    first commit

---
+++ PDC60Module02/PDC60Module02/Model/student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PDC60Module02.Model
+{
+    class student
+    {
+        public string SubjectCode { get; set; }
+        public string SubjectTitle { get; set; } 
+        public int Unit { get; set; }
+
+    }
+}
